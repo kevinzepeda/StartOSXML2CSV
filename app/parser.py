40c38ef2ede4a887283d901d6cb1927e937398b4
kevinzepeda <megakevizaat@gmail.com>
2022-10-13 15:34:40 -0500

    ADD TNL template and function

--- app/parser.py
+++ app/parser.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 
-def parse(filename, temp):
+def StarOSParse(filename, temp):
     """
     Parse xml report StarOS Port Fragments Received Statistics
     Params:
@@ -63,7 +63,7 @@ def parse(filename, temp):
 
 def print_help():
     print('''
-    Parse XML from StarOS Port Fragments Received Statistics
+    Parse XML to CSV
 
     Usage:   python parse.py <filename> <template>
 
@@ -76,10 +76,72 @@ def print_help():
     ------
     <outputfile> : CSV file
     ''')
+
+def standaloneParse(filename, temp):
+    """
+    Standalone Parse XML to CSV
+    Params:
+    _______
+    filename: Str
+            xml file
+    template: Str
+            Fsm file to get data format
+    Return:
+    ------
+    (File): Output file with csv data.
+    """
+    name = "StarOS_Port_Fragments_Received_Statistics"
+    version="v4.0-"
+    author = "Kevin_Zepeda-"
+    date = datetime.today()
+    date = date.strftime("%d%m%Y")
+
+def parseTNL(filename):
+    """
+    Standalone Parse XML to CSV
+    Params:
+    _______
+    filename: Str
+            xml file
+    template: Str
+            Fsm file to get data format
+    Return:
+    ------
+    (File): Output file with csv data.
+    """
+    name = "StarOS_Port_Fragments_Received_Statistics"
+    version="v4.0-"
+    author = "Kevin_Zepeda-"
+    date = datetime.today()
+    date = date.strftime("%d%m%Y")
+
+    template = open('templates/TNL/hw.fsm')
+    input_file = open(filename, encoding='utf-8')
+    raw_text_data = input_file.read()
+    re_table = textfsm.TextFSM(template)
+    fsm_data = re_table.ParseText(raw_text_data)
+    hw = pd.DataFrame(fsm_data, columns=re_table.header)
+
+    template = open('templates/TNL/iprf.fsm')
+    re_table = textfsm.TextFSM(template)
+    fsm_data = re_table.ParseText(raw_text_data)
+    ip = pd.DataFrame(fsm_data, columns=re_table.header)
+
+    hw['IPIF-1'] = hw['distName'].apply(lambda name: ip[(ip['distName'].str.contains(name[:-5])) & (ip['distName'].str.contains('IPIF-1'))].localIpAddr.iloc[0])
+    hw['IPIF-2'] = hw['distName'].apply(lambda name: ip[(ip['distName'].str.contains(name[:-5])) & (ip['distName'].str.contains('IPIF-2'))].localIpAddr.iloc[0])
+    hw = hw.drop(['distName'], axis=1)
+    hw.to_csv(filename + '.csv', index=False)
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 3:
         if os.path.exists(sys.argv[1]) and os.path.exists(sys.argv[2]):
-            parse(sys.argv[1],sys.argv[2])
+            if sys.argv[2] == 'StarOS.fsm':
+                StarOSParse(sys.argv[1], sys.argv[2])
+            else:
+                standaloneParse(sys.argv[1], sys.argv[2])
+        elif os.path.exists(sys.argv[1])  and sys.argv[2] == 'TNL':
+            parseTNL(sys.argv[1])
         else:
             print_help()
     else:
